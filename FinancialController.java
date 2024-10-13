6e5b450c76dc21c447e22a59b754129fbcf9318d
clark-pegg <clarkpegg@hotmail.com>
2024-10-13 15:06:36 -0400

    Added provided files to repository

---
+++ FinancialController.java
@@ -0,0 +1,18 @@
+package com.example.financialdashboard.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class FinancialController {
+
+   @GetMapping("/balances")
+
+   public String getBalances() {
+
+       return "{\"balance\": 1500, \"currency\": \"USD\"}";
+
+   }
+
+}
