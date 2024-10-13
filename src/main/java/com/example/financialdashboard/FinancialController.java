2dbdcdf8fe7dbb3a28a88aaa9bd9c4755bffc3c7
clark-pegg <clarkpegg@hotmail.com>
2024-10-13 17:37:40 -0400

    back-end now reads from JSON file

--- src/main/java/com/example/financialdashboard/FinancialController.java
+++ src/main/java/com/example/financialdashboard/FinancialController.java
@@ -1,14 +1,29 @@
 package com.example.financialdashboard.controller;
 
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.*;
+
 @RestController
 public class FinancialController {
-  
+
   @GetMapping("/balances")
   public String getBalances() {
-    return "{\"balance\": 1500, \"currency\": \"USD\"}";
-  }
+    try{
+      Object object = new JSONParser().parse(new FileReader("balances.json"));
 
+      JSONObject json = (JSONObject) object;
+
+      return json.toString();
+    } catch (Exception e) {
+      e.printStackTrace();
+      return "";
+    }
+  }
 }
