7e35d96a8743754388adbb15c21bf09e29a68120
clark-pegg <clarkpegg@hotmail.com>
2024-10-15 15:10:08 -0400

    moved logic for handling JSON file reading to own file to seperate responsibilites

--- src/main/java/com/example/financialdashboard/controller/FinancialController.java
+++ src/main/java/com/example/financialdashboard/controller/FinancialController.java
@@ -1,29 +1,17 @@
 package com.example.financialdashboard.controller;
 
-import java.io.FileReader;
-import java.io.FileNotFoundException;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.*;
 
 @RestController
 public class FinancialController {
 
+  // DUMMY METHOD
+  // Currently reads from "balances.json", would need to be changed
+  // to read from an actual database
+
   @GetMapping("/balances")
   public String getBalances() {
-    try{
-      Object object = new JSONParser().parse(new FileReader("balances.json"));
-
-      JSONObject json = (JSONObject) object;
-
-      return json.toString();
-    } catch (Exception e) {
-      e.printStackTrace();
-      return "";
-    }
+    return FinancialControllerHelper.getBalanceFromFile();
   }
 }
